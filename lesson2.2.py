45b79409bb5d0df23401ce573e8c1f53dd9b1e30
chenhaobin-bit <3107185467@qq.com>
2025-11-05 11:49:13 +0800

    作业2

--- lesson2.2.py
+++ lesson2.2.py
@@ -2,5 +2,10 @@ import pandas as pd
 df = pd.read_csv("D:\\wps\\xuexi\\flight.csv")
 df1 = df[['date','dist','flight']]
 df2 = df.iloc[[3,4],[0,1]]
+print("df1为")
 print(df1)
-#print(df2)
+print("df2为")
+print(df2)
+df['low_dest'] = df['dest'].str.lower()
+print("添加后的文件为")
+print(df)
---
+++ lesson2.4.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+def pandaun(series):
+    return series.max() - series.min()
+df = pd.read_csv("D:\\wps\\xuexi\\flight.csv")
+df3 = df[(df['dist'] > 1000) & (df['dist'] < 1200)]
+print(df3)
+df4 = df[df['time'] > 100]
+print(df4)
+df5 = pd.concat([df3,df4],axis=0);
+print(df5)
+df6 = df3[['time','dist']]
+print(df6)
+dest_count = df.groupby('dest')['dest'].agg(np.size).rename('dest_count')
+df7 = dest_count.reset_index()[['dest','dest_count']]
+print(df7)
+df5.to_csv("D:\\wps\\xuexi\\df5.csv")
+df7.to_csv("D:\\wps\\xuexi\\df7.csv")
